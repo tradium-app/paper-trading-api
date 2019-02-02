1e7b3517f5758e12fe59ed473682485ea0ffa0c2
Suresh Shrestha <shrestha2lt8@gmail.com>
2019-02-02 23:10:10 +0545

    Added some extra files

--- api/controller/newsController.js
+++ api/controller/newsController.js
@@ -1,1 +1,56 @@
-ECHO is on.
+'use strict';
+
+
+var mongoose = require('mongoose'),
+news = mongoose.model('news');
+
+exports.list_all_news = function(req, res) {
+  news.find({}, function(err, news) {
+    if (err)
+      res.send(err);
+    res.json(news);
+  });
+};
+
+
+
+
+exports.create_a_news = function(req, res) {
+  var new_news = new news(req.body);
+  new_news.save(function(err, news) {
+    if (err)
+      res.send(err);
+    res.json(news);
+  });
+};
+
+
+exports.read_a_news = function(req, res) {
+  news.findById(req.params.newsId, function(err, news) {
+    if (err)
+      res.send(err);
+    res.json(news);
+  });
+};
+
+
+exports.update_a_news= function(req, res) {
+  news.findOneAndUpdate({_id: req.params.newsId}, req.body, {new: true}, function(err, news) {
+    if (err)
+      res.send(err);
+    res.json(news);
+  });
+};
+
+
+exports.delete_a_news = function(req, res) {
+
+
+  news.remove({
+    _id: req.params.newsId
+  }, function(err, news) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'news successfully deleted' });
+  });
+};
---
+++ routes/newsRoute.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(app) {
+  var news = require('../controllers/newsController');
+
+  // news Routes
+  app.route('/news')
+    .get(news.list_all_news)
+    .post(news.create_a_news);
+
+
+  app.route('/news/:newsId')
+    .get(news.read_a_news)
+    .put(news.update_a_news)
+    .delete(news.delete_a_news);
+};
