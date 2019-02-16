04c44d4b8e9f21fef542e169d4dc8193e5702092
Suraj Shrestha <syuraj@gmail.com>
2019-02-16 18:40:12 -0500

    Return only 100 articles in order (#4)
    * return only 100 articles in order
    
    * filter out articles without link
    
    * remove unused ArticleInput for now

--- nepalTodayApi/index.js
+++ nepalTodayApi/index.js
@@ -1,7 +1,7 @@
 const { ApolloServer, gql } = require('apollo-server-azure-functions');
 const fs = require('fs');
 const resolvers = require('../src/database/resolvers.js');
-const { newsDbService } = require('nepaltoday-db-service');
+const { mongooseSchema } = require('nepaltoday-db-service');
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 
@@ -9,6 +9,6 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 const typeDefSchema = fs.readFileSync('./src/database/typeDefs.graphql', 'utf8');
 const typeDefs = gql(typeDefSchema);
 
-const server = new ApolloServer({ typeDefs, resolvers, context: newsDbService });
+const server = new ApolloServer({ typeDefs, resolvers, context: mongooseSchema });
 
 module.exports = server.createHandler();
--- src/database/resolvers.js
+++ src/database/resolvers.js
@@ -1,7 +1,10 @@
 module.exports = {
 	Query: {
-		getArticles: async (parent, args, newsDbService) => {
-			const articles = await newsDbService.getArticles();
+		getArticles: async (parent, args, { Article }) => {
+			const articles = await Article
+				.find({ 'link': { $ne: null } })
+				.sort({ _id: -1 })
+				.limit(100);
 			return articles;
 		}
 	}
