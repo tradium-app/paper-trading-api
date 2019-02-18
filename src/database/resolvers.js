869c587b05bf380a620ff719e70c9800891964ef
Suraj Shrestha <syuraj@gmail.com>
2019-02-18 02:33:24 -0500

    add parameters to getArticles (#5)

--- src/database/resolvers.js
+++ src/database/resolvers.js
@@ -1,8 +1,16 @@
 module.exports = {
 	Query: {
 		getArticles: async (parent, args, { Article }) => {
+			args.criteria = args.criteria || {}
+			args.criteria.lastQueryDate = args.criteria.lastQueryDate || new Date('2001-01-01')
+			args.criteria.lastArticleId = args.criteria.lastArticleId || '000000000000000000000000'
+
 			const articles = await Article
-				.find({ 'link': { $ne: null } })
+				.find({
+					'link': { $ne: null },
+					'modifiedDate': { $gt: new Date(args.criteria.lastQueryDate) },
+					'_id': { $gt: args.criteria.lastArticleId }
+				})
 				.sort({ _id: -1 })
 				.limit(100);
 			return articles;
