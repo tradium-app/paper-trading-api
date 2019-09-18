426f82f343e0534c7aafbdfea48511c7f5a175a0
shrijan-siris <54256789+shrijan-siris@users.noreply.github.com>
2019-09-18 14:15:18 +0545

    Feature/write sort function (#20)
    * fields are added
    
    * feature/write-sort-function: generic sort function with test case added

---
+++ src/utils/arrayUtil.js
@@ -0,0 +1,9 @@
+const sortArrayByWeight = (arr = []) => {
+  return arr.sort((a, b) =>
+    a.weight > b.weight ? -1 : b.weight > a.weight ? 1 : 0
+  );
+};
+
+module.exports = {
+  sortArrayByWeight
+};
---
+++ src/utils/arryUtil.test.js
@@ -0,0 +1,22 @@
+const { sortArrayByWeight } = require("./arrayUtil");
+
+describe("Array Util Test", () => {
+  it("sort array by weight", () => {
+    const arr = [
+      { source: "Kantipur", weight: 50 },
+      { source: "Ratopati", weight: 60 },
+      { source: "Setopati", weight: 70 },
+      { source: "Hariyopati", weight: 70 }
+    ];
+    const expected = [
+      { source: "Setopati", weight: 70 },
+      { source: "Hariyopati", weight: 70 },
+      { source: "Ratopati", weight: 60 },
+      { source: "Kantipur", weight: 50 }
+    ];
+
+    const sortedArr = sortArrayByWeight(arr);
+    console.log("sorted Arr", sortedArr);
+    expect(JSON.stringify(sortedArr)).toBe(JSON.stringify(expected));
+  });
+});
