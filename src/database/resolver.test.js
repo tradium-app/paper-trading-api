7615f06d4d3de2f0892858c7901168309a12c584
Suraj Shrestha <syuraj@gmail.com>
2019-10-01 18:05:11 -0400

    Adding eslint/prettier formatting config (#26)

--- src/database/resolver.test.js
+++ src/database/resolver.test.js
@@ -1,6 +1,5 @@
-
-describe("resolver test demo", () => {
-  it("demo test", () => {
-    expect(2 + 2).toBe(4);
-  });
-});
+describe('resolver test demo', () => {
+	it('demo test', () => {
+		expect(2 + 2).toBe(4)
+	})
+})
--- src/database/resolvers.js
+++ src/database/resolvers.js
@@ -1,39 +1,43 @@
-const { getSortedArticle } = require("../helper/articleHelper");
-const { getSortedTweets } = require("../helper/twitterHelper");
+const { getSortedArticle } = require('../helper/articleHelper')
+const { getSortedTweets } = require('../helper/twitterHelper')
+
 exports.resolver = {
-  Query: {
-    getArticles: async (parent, args, { Article }) => {
-      args.criteria = args.criteria || {};
-      args.criteria.lastQueryDate =
-        args.criteria.lastQueryDate || new Date("2001-01-01");
-      args.criteria.lastArticleId =
-        args.criteria.lastArticleId || "000000000000000000000000";
+	Query: {
+		getArticles: async (parent, args, { Article }) => {
+			args.criteria = args.criteria || {}
+			args.criteria.lastQueryDate = args.criteria.lastQueryDate || new Date('2001-01-01')
+			args.criteria.lastArticleId = args.criteria.lastArticleId || '000000000000000000000000'
 
-      const articles = await Article.find({
-        link: { $ne: null },
-        modifiedDate: { $gt: new Date(args.criteria.lastQueryDate) },
-        _id: { $gt: args.criteria.lastArticleId }
-      })
-        .populate("source")
-        .sort({ _id: -1 })
-        .limit(100);
-      const sortedArticles = getSortedArticle(articles);
-      return sortedArticles;
-    },
-    getTweets: async (parent, args, { Tweet }) => {
-      args.criteria = args.criteria || {};
-      args.criteria.lastQueryDate =
-        args.criteria.lastQueryDate || new Date("2001-01-01");
-      args.criteria.lastTweetId =
-        args.criteria.lastTweetId || "000000000000000000000000";
+			const articles = await Article.find({
+				link: { $ne: null },
+				modifiedDate: { $gt: new Date(args.criteria.lastQueryDate) },
+				_id: { $gt: args.criteria.lastArticleId },
+			})
+				.populate('source')
+				.sort({ _id: -1 })
+				.limit(100)
 
-      const tweets = await Tweet.find()
-        .populate("twitterHandle")
-        .sort({ publishedDate: -1 })
-        .limit(100);
+			console.log('Printing articles before sorting', articles.map(a => a.title))
 
-      //   const sortedTweets = getSortedTweets(tweets);
-      return tweets;
-    }
-  }
-};
+			const sortedArticles = getSortedArticle(articles)
+
+			console.log('Printing articles after sorting', sortedArticles.map(a => a.title))
+
+			return sortedArticles
+		},
+
+		getTweets: async (parent, args, { Tweet }) => {
+			args.criteria = args.criteria || {}
+			args.criteria.lastQueryDate = args.criteria.lastQueryDate || new Date('2001-01-01')
+			args.criteria.lastTweetId = args.criteria.lastTweetId || '000000000000000000000000'
+
+			const tweets = await Tweet.find()
+				.populate('twitterHandle')
+				.sort({ publishedDate: -1 })
+				.limit(100)
+
+			//   const sortedTweets = getSortedTweets(tweets);
+			return tweets
+		},
+	},
+}
