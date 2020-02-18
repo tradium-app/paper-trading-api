f7497712fb1c85f9540508182da3ac8ea47039cc
chetannn <chetankharel7@gmail.com>
2020-02-18 21:02:18 +0545

    update source-data file

--- src/db-service/scripts/source-data.js
+++ src/db-service/scripts/source-data.js
@@ -1,147 +1,164 @@
 /* eslint-disable no-undef */
 
-db.sources.insert({
-	name: 'सेतोपाटी',
-	link: 'https://setopati.com',
-	logoLink: 'https://setopati.com/images/og-image.jpg',
-	category: [
-		{
-			name: 'politics',
-			path: '/politics',
-		},
-		{
-			name: 'social',
-			path: '/social',
-		},
-		{
-			name: 'opinion',
-			path: '/opinion',
-		},
-		{
-			name: 'sports',
-			path: '/sports',
-		},
-		{
-			name: 'entertainment',
-			path: '/art',
-		},
-	],
-})
-db.sources.insert({
-	name: 'दैनिक नेपाल',
-	link: 'https://dainiknepal.com',
-	logoLink: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzh7lprLde3-7eyyxDMPVhfSJkKEHn6N7qDMz6_KwUpd6pC8U8',
-	category: [
-		{
-			name: 'news',
-			path: '/section/latest-news',
-		},
-		{
-			name: 'opinion',
-			path: '/section/views',
-		},
-		{
-			name: 'sports',
-			path: '/section/sports',
-		},
-		{
-			name: 'entertainment',
-			path: '/section/kala',
-		},
+const sourceList = [
+	{
+		name: 'सेतोपाटी',
+		link: 'https://setopati.com',
+		logoLink: 'https://setopati.com/images/og-image.jpg',
+		category: [
+			{
+				name: 'politics',
+				path: '/politics',
+			},
+			{
+				name: 'social',
+				path: '/social',
+			},
+			{
+				name: 'opinion',
+				path: '/opinion',
+			},
+			{
+				name: 'sports',
+				path: '/sports',
+			},
+			{
+				name: 'entertainment',
+				path: '/art',
+			},
+		],
+	},
+	{
+		name: 'दैनिक नेपाल',
+		link: 'https://dainiknepal.com',
+		logoLink: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzh7lprLde3-7eyyxDMPVhfSJkKEHn6N7qDMz6_KwUpd6pC8U8',
+		category: [
+			{
+				name: 'news',
+				path: '/section/latest-news',
+			},
+			{
+				name: 'opinion',
+				path: '/section/views',
+			},
+			{
+				name: 'sports',
+				path: '/section/sports',
+			},
+			{
+				name: 'entertainment',
+				path: '/section/kala',
+			},
 
-		{
-			name: 'business',
-			path: '/kinmel',
-		},
-	],
-})
-db.sources.insert({
-	name: 'कान्तिपुर',
-	link: 'https://ekantipur.com',
-	logoLink: 'https://pbs.twimg.com/profile_images/943652378875535360/K81bnaWT_400x400.jpg',
-	category: [
-		{
-			name: 'news',
-			path: '/news',
-		},
-		{
-			name: 'business',
-			path: '/business',
-		},
-		{
-			name: 'opinion',
-			path: '/opinion',
-		},
-		{
-			name: 'sports',
-			path: '/sports',
-		},
-		{
-			name: 'entertainment',
-			path: '/entertainment',
-		},
-	],
-})
-db.sources.insert({
-	name: 'रातोपाटी',
-	link: 'https://ratopati.com',
-	logoLink: 'https://ratopati.prixa.net/static/normal/img/ratopati_default.jpg',
-	category: [
-		{
-			name: 'news',
-			path: '/category/news',
-		},
-		{
-			name: 'business',
-			path: '/economy',
-		},
-		{
-			name: 'opinion',
-			path: '/category/opinion',
-		},
-		{
-			name: 'sports',
-			path: '/category/sports',
-		},
-		{
-			name: 'entertainment',
-			path: '/entertainment',
-		},
-	],
-})
+			{
+				name: 'business',
+				path: '/kinmel',
+			},
+		],
+	},
+	{
+		name: 'कान्तिपुर',
+		link: 'https://ekantipur.com',
+		logoLink: 'https://pbs.twimg.com/profile_images/943652378875535360/K81bnaWT_400x400.jpg',
+		category: [
+			{
+				name: 'news',
+				path: '/news',
+			},
+			{
+				name: 'business',
+				path: '/business',
+			},
+			{
+				name: 'opinion',
+				path: '/opinion',
+			},
+			{
+				name: 'sports',
+				path: '/sports',
+			},
+			{
+				name: 'entertainment',
+				path: '/entertainment',
+			},
+		],
+	},
+	{
+		name: 'रातोपाटी',
+		link: 'https://ratopati.com',
+		logoLink: 'https://ratopati.prixa.net/static/normal/img/ratopati_default.jpg',
+		category: [
+			{
+				name: 'news',
+				path: '/category/news',
+			},
+			{
+				name: 'business',
+				path: '/economy',
+			},
+			{
+				name: 'opinion',
+				path: '/category/opinion',
+			},
+			{
+				name: 'sports',
+				path: '/category/sports',
+			},
+			{
+				name: 'entertainment',
+				path: '/entertainment',
+			},
+		],
+	},
+	{
+		name: 'अनलाईन खबर',
+		link: 'https://onlinekhabar.com',
+		logoLink: 'https://www.onlinekhabar.com/wp-content/themes/onlinekhabar-2018/img/logoMainWht.png',
+		category: [
+			{
+				name: 'business',
+				path: '/business',
+			},
+			{
+				name: 'opinion',
+				path: '/category/opinion',
+			},
+			{
+				name: 'sports',
+				path: '/sports',
+			},
+			{
+				name: 'entertainment',
+				path: '/entertainment',
+			},
+		],
+	},
+	{
+		name: 'BBC नेपाली',
+		link: 'https://bbc.com/nepali',
+		logoLink: 'https://news.files.bbci.co.uk/ws/img/logos/og/nepali.png',
+		category: [
+			{
+				name: 'news',
+				path: '/news',
+			},
+		],
+	},
+]
 
-db.sources.insert({
-	name: 'अनलाईन खबर',
-	link: 'https://onlinekhabar.com',
-	logoLink: 'https://www.onlinekhabar.com/wp-content/themes/onlinekhabar-2018/img/logoMainWht.png',
-	category: [
-		{
-			name: 'business',
-			path: '/business',
-		},
-		{
-			name: 'opinion',
-			path: '/category/opinion',
-		},
-		{
-			name: 'sports',
-			path: '/sports',
-		},
-		{
-			name: 'entertainment',
-			path: '/entertainment',
-		},
-	],
-})
-
-db.sources.insert({
-	name: 'BBC नेपाली',
-	link: 'https://bbc.com/nepali',
-	logoLink: 'https://news.files.bbci.co.uk/ws/img/logos/og/nepali.png',
-	category: [
-		{
-			name: 'news',
-			path: '/news',
-		},
-	],
+sourceList.forEach(function(source) {
+	let sourceDoc = db.sources.findOne({ link: source.link })
+	if (sourceDoc) {
+		if (sourceDoc.link == source.link) {
+			//update
+			print('____________Updating existing source_________')
+			print(sourceDoc.link)
+			db.sources.update({ _id: sourceDoc._id }, source)
+		}
+	} else {
+		//insert
+		print('___________Inserting new source____________')
+		print(source.link)
+		db.sources.insert(source)
+	}
 })
