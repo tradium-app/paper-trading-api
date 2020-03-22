b2b598dda3ea23edcf49ff73b1b313ba12e09175
Suraj Shrestha <syuraj@gmail.com>
2020-03-22 16:49:10 -0400

    missing file

---
+++ src/db-service/tests/test-db-server.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const { MongoMemoryServer } = require('mongodb-memory-server')
+
+const mongod = new MongoMemoryServer()
+
+module.exports = {
+	connect: async () => {
+		const uri = await mongod.getConnectionString()
+
+		await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+	},
+
+	closeDatabase: async () => {
+		await mongoose.connection.dropDatabase()
+		await mongoose.connection.close()
+		await mongod.stop()
+	},
+
+	clearDatabase: async () => {
+		const collections = mongoose.connection.collections
+
+		for (const key in collections) {
+			const collection = collections[key]
+			await collection.deleteMany()
+		}
+	},
+}
