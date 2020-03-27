dfa6dcb908fb0b6288b48a3f8789d55c8d1c9c84
Suraj Shrestha <syuraj@gmail.com>
2020-03-26 23:49:38 -0400

    adding integration test for PushNotificationSender

---
+++ src/jobs/NotificationTrigger/__tests__/pushNotificationSender.integration.js
@@ -0,0 +1,21 @@
+const { sendPushNotification } = require('../pushNotificationSender')
+const { newsDbService } = require('../../../db-service/')
+require('dotenv').config()
+
+describe('PushNotificationSender integration', () => {
+	it('single notification should go successfullly', async () => {
+		const latestArticle = await newsDbService.getLatestNewsArticle()
+
+		const oneNotification = {
+			notification: {
+				title: latestArticle[0].title,
+				body: latestArticle[0].shortDescription
+			},
+			to: process.env.TEST_USER_FCM_TOKEN,
+			data: latestArticle[0]
+		}
+		let notificationSentStatus = await sendPushNotification(oneNotification)
+
+		expect(notificationSentStatus.status).toBeTruthy()
+	})
+})
