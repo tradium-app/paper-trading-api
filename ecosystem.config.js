f2a7091444a872fe0bb02c1072b637ed08b77b76
Suraj Shrestha <syuraj@gmail.com>
2020-03-29 12:34:46 -0400

    ignore watch non-source files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,6 +8,7 @@ module.exports = {
 			instances: process.env.APP_INSTANCES || 'max',
 			autorestart: true,
 			watch: process.env.NODE_ENV == 'development' ? true : false,
+			ignore_watch: ['node_modules', 'assets', 'docs', 'newrelic_agent.log', '.git'],
 			max_memory_restart: process.env.APP_MEMORY_LIMIT || '2G',
 		},
 	],
