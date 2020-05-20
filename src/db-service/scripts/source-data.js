ebcb7fb76fe126b4cb7cfc2311b46ca2204b04cd
Suraj Shrestha <syuraj@gmail.com>
2020-05-19 21:28:36 -0400

    dainiknepal url fix

--- src/db-service/scripts/source-data.js
+++ src/db-service/scripts/source-data.js
@@ -3,145 +3,145 @@ const sourceList = [
 	{
 		name: 'सेतोपाटी',
 		link: 'https://setopati.com',
-		logoLink: "/assets/logos/setopati.jpg",
+		logoLink: '/assets/logos/setopati.jpg',
 		category: [
 			{
 				name: 'politics',
-				path: '/politics'
+				path: '/politics',
 			},
 			{
 				name: 'social',
-				path: '/social'
+				path: '/social',
 			},
 			{
 				name: 'opinion',
-				path: '/opinion'
+				path: '/opinion',
 			},
 			{
 				name: 'sports',
-				path: '/sports'
+				path: '/sports',
 			},
 			{
 				name: 'entertainment',
-				path: '/art'
-			}
-		]
+				path: '/art',
+			},
+		],
 	},
 	{
 		name: 'दैनिक नेपाल',
-		link: 'https://dainiknepal.com',
-		logoLink: "/assets/logos/dainik.png",
+		link: 'https://www.dainiknepal.com',
+		logoLink: '/assets/logos/dainik.png',
 		category: [
 			{
 				name: 'news',
-				path: '/section/latest-news'
+				path: '/section/latest-news',
 			},
 			{
 				name: 'opinion',
-				path: '/section/views'
+				path: '/section/views',
 			},
 			{
 				name: 'sports',
-				path: '/section/sports'
+				path: '/section/sports',
 			},
 			{
 				name: 'entertainment',
-				path: '/section/kala'
+				path: '/section/kala',
 			},
 
 			{
 				name: 'business',
-				path: '/section/market'
-			}
-		]
+				path: '/section/market',
+			},
+		],
 	},
 	{
 		name: 'कान्तिपुर',
 		link: 'https://ekantipur.com',
-		logoLink: "/assets/logos/kantipur.jpg",
+		logoLink: '/assets/logos/kantipur.jpg',
 		category: [
 			{
 				name: 'news',
-				path: '/news'
+				path: '/news',
 			},
 			{
 				name: 'business',
-				path: '/business'
+				path: '/business',
 			},
 			{
 				name: 'opinion',
-				path: '/opinion'
+				path: '/opinion',
 			},
 			{
 				name: 'sports',
-				path: '/sports'
+				path: '/sports',
 			},
 			{
 				name: 'entertainment',
-				path: '/entertainment'
-			}
-		]
+				path: '/entertainment',
+			},
+		],
 	},
 	{
 		name: 'रातोपाटी',
 		link: 'https://ratopati.com',
-		logoLink: "/assets/logos/ratopati.png",
+		logoLink: '/assets/logos/ratopati.png',
 		category: [
 			{
 				name: 'news',
-				path: '/category/news'
+				path: '/category/news',
 			},
 			{
 				name: 'business',
-				path: '/economy'
+				path: '/economy',
 			},
 			{
 				name: 'opinion',
-				path: '/category/opinion'
+				path: '/category/opinion',
 			},
 			{
 				name: 'sports',
-				path: '/category/sports'
+				path: '/category/sports',
 			},
 			{
 				name: 'entertainment',
-				path: '/entertainment'
-			}
-		]
+				path: '/entertainment',
+			},
+		],
 	},
 	{
 		name: 'अनलाईन खबर',
 		link: 'https://onlinekhabar.com',
-		logoLink: "/assets/logos/online-khabar.png",
+		logoLink: '/assets/logos/online-khabar.png',
 		category: [
 			{
 				name: 'business',
-				path: '/business'
+				path: '/business',
 			},
 			{
 				name: 'sports',
-				path: '/sports'
+				path: '/sports',
 			},
 			{
 				name: 'entertainment',
-				path: '/entertainment'
-			}
-		]
+				path: '/entertainment',
+			},
+		],
 	},
 	{
 		name: 'BBC नेपाली',
 		link: 'https://bbc.com/nepali',
-		logoLink: "/assets/logos/bbc.png",
+		logoLink: '/assets/logos/bbc.png',
 		category: [
 			{
 				name: 'news',
-				path: ''
-			}
-		]
-	}
+				path: '',
+			},
+		],
+	},
 ]
 
-sourceList.forEach(function(source) {
+sourceList.forEach(function (source) {
 	print('____________Updating existing source and inserting new source if not exist_________')
 	printjson(source)
 	db.sources.update({ link: source.link }, source, { upsert: true })
