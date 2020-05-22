115f9d70230ede2c1c0b20b9098e73c3d33aef18
Suraj Shrestha <syuraj@gmail.com>
2020-05-22 11:49:13 -0400

    log error when notification send fails

--- src/jobs/NotificationTrigger/pushNotificationSender.js
+++ src/jobs/NotificationTrigger/pushNotificationSender.js
@@ -1,18 +1,21 @@
 const { post } = require('./http')
+const logger = require('../../config/logger')
 
 const sendPushNotification = async (notification) => {
 	try {
 		const response = await post(undefined, notification)
-		if (response.status === 200) {
+		if (response.status === 200 && response.data.success === 1) {
 			return { status: true, success: response.data.success, failure: response.data.failure }
 		} else {
+			logger.error(response.data.results)
 			return { status: false }
 		}
 	} catch (error) {
+		logger.error(error)
 		return { status: false, message: `Notification send failed: ${error.stack}` }
 	}
 }
 
 module.exports = {
-	sendPushNotification
+	sendPushNotification,
 }
