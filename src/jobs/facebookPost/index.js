fa0659f6cdaee1ccd44331d70419708e4fe3d157
Shiva Aryal <lostinhell99@gmail.com>
2020-08-31 13:43:30 +0545

    NEP-219 : Crawl time increased (#100)
    * fb puppeter library from news crawler library
    
    * CrawlTimeIncreased
    
    Co-authored-by: Shiva Aryal <shivaaryalj7@gmail.com>

--- src/jobs/facebookPost/index.js
+++ src/jobs/facebookPost/index.js
@@ -1,50 +1,39 @@
 const { newsDbService } = require('../../db-service')
-const { verifyFacebookPostTime } = require('./facebookPostHelper')
 const getBrowser = require('news-crawler/src/get-browser')
 require('dotenv').config()
 
-module.exports = async function (context) {
-	try {
-		const verifyPostEligibleTime = verifyFacebookPostTime()
-		if (verifyPostEligibleTime) {
-			context.log('Posting to fb...')
-			const latestArticle = await newsDbService.getLatestNewsArticle()
-			const articleLink = latestArticle[0].link
+module.exports = async function(context){
 
-			const browser = await getBrowser()
-			const browserPage = await browser.newPage()
-			await browserPage.setDefaultNavigationTimeout(100000)
-			await browserPage.goto(process.env.FACEBOOK_PAGE_LINK)
-
-			await browserPage.waitForSelector('#email')
-			await browserPage.type('#email', process.env.FACEBOOK_EMAIL_ID)
-
-			await browserPage.type('#pass', process.env.FACEBOOK_PASSWORD)
-
-			await browserPage.click(`[type="submit"]`)
-			await browserPage.waitForNavigation()
-
-			await browserPage.waitForSelector(`[aria-label="Write a post..."]`)
-			await browserPage.click(`[aria-label="Write a post..."]`)
-
-			// await browserPage.waitForSelector('div.g9en0fbe > div > img')
-
-			for (let i = 0; i < articleLink.length; i++) {
-				await browserPage.keyboard.press(articleLink[i])
-
-				if (i === articleLink.length - 1) {
-					await browserPage.waitFor(2000)
-					await browserPage.keyboard.down('Control')
-					await browserPage.keyboard.press(String.fromCharCode(13))
-					await browserPage.keyboard.up('Control')
-					await browserPage.waitFor(10000)
-					context.log('Posted to FB')
-				}
-			}
-
-			browserPage.close()
-		}
-	} catch (error) {
-		context.log('error here fb post', error)
-	}
+    try {
+        context.log("Posting to fb...")
+        const latestArticle = await newsDbService.getLatestNewsArticle()
+        let articleLink = latestArticle[0].link
+        
+        const browser = await getBrowser()
+	      const browserPage = await browser.newPage()
+        await browserPage.setDefaultNavigationTimeout(100000)
+        await browserPage.goto(process.env.FACEBOOK_PAGE_LINK)
+        await browserPage.waitForSelector('#email')
+        await browserPage.type("#email", process.env.FACEBOOK_EMAIL_ID)
+        await browserPage.type('#pass', process.env.FACEBOOK_PASSWORD)
+        await browserPage.click(`[type="submit"]`)
+        await browserPage.waitForNavigation()
+        await browserPage.waitForSelector(`[aria-label="Write a post..."]`)
+        await browserPage.click(`[aria-label="Write a post..."]`)
+        // await browserPage.waitForSelector('div.g9en0fbe > div > img')
+        for(let i = 0; i < articleLink.length; i++){
+            await browserPage.keyboard.press(articleLink[i])
+            if(i === articleLink.length-1){
+                await browserPage.waitFor(2000)
+                await browserPage.keyboard.down('Control')
+                await browserPage.keyboard.press(String.fromCharCode(13))
+                await browserPage.keyboard.up('Control')
+                await browserPage.waitFor(10000)
+                context.log("Posted to FB")
+            }
+        }
+        browserPage.close()
+    } catch (error) {
+        context.log("error here fb post",error)
+    }
 }
--- src/jobs/job-runner/start-jobs.js
+++ src/jobs/job-runner/start-jobs.js
@@ -52,11 +52,11 @@ module.exports = async function () {
 
 	await agenda.start()
 
-	await agenda.every('10 minutes', 'crawl articles')
+	await agenda.every('30 minutes', 'crawl articles')
 	await agenda.every('5 minutes', 'notify users')
 	await agenda.every('10 minutes', 'pull tweets')
 	await agenda.every('2 hours', 'fetch corona stats')
 	await agenda.every('2 hours', 'fetch district corona stats')
 	await agenda.every('1 hours', 'fetch trending')
-	await agenda.every('5 minutes', 'post to facebook')
+	await agenda.every('136 minutes', 'post to facebook')
 }
