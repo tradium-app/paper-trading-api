058d54b0d72d9c10efccb5c5c44a5ae025605161
Suraj Shrestha <syuraj@gmail.com>
2021-02-15 03:17:02 -0500

    minor warning fix

--- packages/admin/src/components/VerticalLayout/Header.js
+++ packages/admin/src/components/VerticalLayout/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from "react";
 
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
@@ -13,11 +13,14 @@ import logoLightSvg from "../../assets/images/logo-light.svg";
 import logoDark from "../../assets/images/logo-dark.png";
 
 // Redux Store
-import {  showRightSidebarAction,toggleLeftmenu,changeSidebarType } from "../../store/actions";
+import {
+  showRightSidebarAction,
+  toggleLeftmenu,
+  changeSidebarType,
+} from "../../store/actions";
 
 const Header = (props) => {
-
-  const isMobile =  /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
   function toggleFullscreen() {
     if (
       !document.fullscreenElement &&
@@ -45,64 +48,85 @@ const Header = (props) => {
     }
   }
 
-function tToggle()
-{
-       props.toggleLeftmenu(!props.leftMenu);
-      if (props.leftSideBarType === "default") {
-         props.changeSidebarType("condensed", isMobile);
+  function tToggle() {
+    props.toggleLeftmenu(!props.leftMenu);
+    if (props.leftSideBarType === "default") {
+      props.changeSidebarType("condensed", isMobile);
     } else if (props.leftSideBarType === "condensed") {
-         props.changeSidebarType("default", isMobile);
-    }  
-}
-      return (
-       <React.Fragment>
-        <header id="page-topbar">
-          <div className="navbar-header">
-            <div className="d-flex">
-              <div className="navbar-brand-box">
-                <Link to="/" className="logo logo-dark">
-                  <span className="logo-sm">
-                    <img src={logo} alt="" height="22" />
-                  </span>
-                  <span className="logo-lg">
-                    <img src={logoDark} alt="" height="17" />
-                  </span>
-                </Link>
+      props.changeSidebarType("default", isMobile);
+    }
+  }
+  return (
+    <React.Fragment>
+      <header id="page-topbar">
+        <div className="navbar-header">
+          <div className="d-flex">
+            <div className="navbar-brand-box">
+              <Link to="/" className="logo logo-dark">
+                <span className="logo-sm">
+                  <img src={logo} alt="" height="22" />
+                </span>
+                <span className="logo-lg">
+                  <img src={logoDark} alt="" height="17" />
+                </span>
+              </Link>
 
-                <Link to="/" className="logo logo-light">
-                  <span className="logo-sm">
-                    <img src={logoLightSvg} alt="" height="22" />
-                  </span>
-                  <span className="logo-lg">
-                    <img src={logoLightPng} alt="" height="19" />
-                  </span>
-                </Link>
-              </div>
+              <Link to="/" className="logo logo-light">
+                <span className="logo-sm">
+                  <img src={logoLightSvg} alt="" height="22" />
+                </span>
+                <span className="logo-lg">
+                  <img src={logoLightPng} alt="" height="19" />
+                </span>
+              </Link>
+            </div>
 
-              <button type="button" onClick={() => {   tToggle() }} className="btn btn-sm px-3 font-size-16 header-item waves-effect" id="vertical-menu-btn">
-                <i className="fa fa-fw fa-bars"></i>
+            <button
+              type="button"
+              onClick={() => {
+                tToggle();
+              }}
+              className="btn btn-sm px-3 font-size-16 header-item waves-effect"
+              id="vertical-menu-btn"
+            >
+              <i className="fa fa-fw fa-bars"></i>
+            </button>
+          </div>
+
+          <div className="d-flex">
+            <div className="dropdown d-none d-lg-inline-block ml-1">
+              <button
+                type="button"
+                onClick={() => {
+                  toggleFullscreen();
+                }}
+                className="btn header-item noti-icon waves-effect"
+                data-toggle="fullscreen"
+              >
+                <i className="bx bx-fullscreen"></i>
               </button>
             </div>
 
-            <div className="d-flex">
-              <div className="dropdown d-none d-lg-inline-block ml-1">
-                <button type="button" onClick={() => { toggleFullscreen(); }} className="btn header-item noti-icon waves-effect" data-toggle="fullscreen">
-                  <i className="bx bx-fullscreen"></i>
-                </button>
-              </div>
-
-              <NotificationDropdown />
-              <ProfileMenu />
-
-            </div>
+            <NotificationDropdown />
+            <ProfileMenu />
           </div>
-        </header>
-      </React.Fragment>
-      );
-    }
-const mapStatetoProps = state => {
-  const { layoutType,showRightSidebar,leftMenu,leftSideBarType } = state.Layout;
-  return { layoutType,showRightSidebar,leftMenu,leftSideBarType };
+        </div>
+      </header>
+    </React.Fragment>
+  );
+};
+const mapStatetoProps = (state) => {
+  const {
+    layoutType,
+    showRightSidebar,
+    leftMenu,
+    leftSideBarType,
+  } = state.Layout;
+  return { layoutType, showRightSidebar, leftMenu, leftSideBarType };
 };
 
-export default connect(mapStatetoProps, { showRightSidebarAction,toggleLeftmenu,changeSidebarType })(Header);
+export default connect(mapStatetoProps, {
+  showRightSidebarAction,
+  toggleLeftmenu,
+  changeSidebarType,
+})(Header);
