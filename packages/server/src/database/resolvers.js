dd57d4d5d5c90f4f1ee499eb284dd63c91ba6f26
Suraj Shrestha <syuraj@gmail.com>
2021-03-26 01:48:38 -0400

    cleanup; modified graphql schema from old app to new hypergrowth app

--- packages/server/src/database/resolvers.js
+++ packages/server/src/database/resolvers.js
@@ -2,20 +2,15 @@
 const _ = require('lodash')
 const mongooseSchema = require('../db-service/database/mongooseSchema')
 
-const { User, Article, Like, Dislike } = mongooseSchema
+const { User } = mongooseSchema
 const { categories } = require('../config/category')
-const getWeather = require('../weather')
 const logger = require('../config/logger')
-const { Tweet, FavoriteFM, ReadArticle } = require('../db-service/database/mongooseSchema')
 const SourceConfig = require('../config/news-source-config.json')
-const { fmDetails } = require('./../config/fm')
 const { calculateTotalWeights } = require('./calculateTotalWeights')
-const { NepaliEvents } = require('../config/nepaliCalender')
-const { getTwitterHandles } = require('../db-service/TweetDbService')
 
 module.exports = {
 	Query: {
-		getArticles: async (parent, args, { Article }) => {
+		getStockNews: async (parent, args, { Article }) => {
 			args.criteria = args.criteria || {}
 			args.criteria.lastQueryDate = args.criteria.lastQueryDate || new Date('2001-01-01')
 			args.criteria.lastArticleId = args.criteria.lastArticleId || '000000000000000000000000'
@@ -55,207 +50,13 @@ module.exports = {
 			return articleList
 		},
 
-		getArticle: async (parent, { _id }) => {
-			const article = await Article.findById(_id).lean()
-
-			if (article == null) {
-				logger.warn(`Article Id ${_id} not found in db`)
-				return null
-			}
-
-			const source = SourceConfig.find((x) => x.sourceName === article.sourceName)
-			return {
-				...article,
-				source: { name: source.nepaliName, url: source.link, logoLink: process.env.SERVER_BASE_URL + source.logoLink },
-			}
+		getWatchList: async (parent, { _id }) => {
+			return [{}]
 		},
-
-		getIndividualArticles: async (parent, { name }) => {
-			const articles = await Article.find({ tags: name }).lean().sort({ _id: -1 })
-			const individualHandle = (await getTwitterHandles()).filter(x => x.nepaliName==name)[0]
-			let articleFlattened = _.flatten(articles)
-			if(individualHandle){
-				const individualNewsCategories = individualHandle.newsCategories
-				articleFlattened = articleFlattened.filter(x => individualNewsCategories.includes(x.category)).slice(0,20)
-			}
-			const articleList = articleFlattened.map((article) => {
-				const mySource = SourceConfig.find((x) => x.sourceName === article.sourceName)
-				article.source = {
-					_id: mySource.name,
-					name: mySource.nepaliName,
-					url: mySource.link,
-					logoLink: process.env.SERVER_BASE_URL + mySource.logoLink,
-				}
-				return article
-			})
-			return articleList
-		},
-
-		getArticlesFromTag: async (parent, { tag }) => {
-			const articles = await Article.find({ tags: tag }).lean().sort({ _id: -1 }).limit(20)
-			let articleFlattened = _.flatten(articles)
-			const articleList = articleFlattened.map((article) => {
-				const mySource = SourceConfig.find((x) => x.sourceName === article.sourceName)
-				article.source = {
-					_id: mySource.name,
-					name: mySource.nepaliName,
-					url: mySource.link,
-					logoLink: process.env.SERVER_BASE_URL + mySource.logoLink,
-				}
-				return article
-			})
-			return articleList
-		},
-
-		getTweets: async (parent, args, { Tweet }) => {
-			args.criteria = args.criteria || {}
-			args.criteria.lastQueryDate = args.criteria.lastQueryDate || new Date('2001-01-01')
-			args.criteria.lastTweetId = args.criteria.lastTweetId || '000000000000000000000000'
-
-			const tweets = await Tweet.find().lean().sort({ publishedDate: -1 }).limit(100)
-			const tweetsWithHandle = tweets.map((t) => {
-				return { ...t, twitterHandle: { name: t.handle, handle: t.handle, _id: t._id } }
-			})
-
-			return tweetsWithHandle
-		},
-
-		getTweetByHandle: async (parent, { handle }) => {
-			if (handle.charAt(0) !== '@') handle = '@' + handle
-			const tweets = await Tweet.find({ handle }).sort({ publishedDate: -1 }).limit(100)
-			return tweets
-		},
-
-		getLatestCoronaStats: async (parent, args, { CoronaStats }) => {
-			const { CoronaDbService } = require('../db-service')
-			return await CoronaDbService.getLatestStats()
-		},
-
-		getDistrictCoronaStats: async (parent, args, { DistrictCoronaStats }) => {
-			const { DistrictCoronaDbService } = require('../db-service')
-			return await DistrictCoronaDbService.getDistrictCoronaStats()
-		},
-
-		getWeatherInfo: async (parent, args, { ipAddress }) => {
-			try {
-				if (ipAddress === '::1' || ipAddress === '::ffff:127.0.0.1') ipAddress = '27.111.16.0'
-				logger.debug(`Printing ip: ${ipAddress}`)
-
-				const weatherInfo = await getWeather(ipAddress)
-				weatherInfo.ipAddress = ipAddress
-
-				return {
-					ipAddress: ipAddress,
-					temperature: weatherInfo.main.temp,
-					condition: weatherInfo.weather[0].main,
-					description: weatherInfo.weather[0].description,
-					place: weatherInfo.name,
-				}
-			} catch (error) {
-				logger.error(`Error to getWeatherInfo for ip: ${ipAddress}`)
-			}
-		},
-
-		getFmList: async (parent, args, { FM }) => {
-			return fmDetails
-		},
-
-		getMyFavoriteFm: async (parent, { nid }) => {
-			const myFavorites = await FavoriteFM.find({ nid })
-			const myFavoriteFm = []
-			myFavorites.forEach((favorite) => {
-				const myFm = fmDetails.find((x) => x.id == favorite.fmId)
-				myFm && myFavoriteFm.push(myFm)
-			})
-			return myFavoriteFm
-		},
-
-		getMyFm: async (parent, { nid }) => {
-			const myFavorites = await FavoriteFM.find({ nid })
-			const myFavoriteFm = []
-			myFavorites.forEach((favorite) => {
-				const myFm = fmDetails.find((x) => x.id == favorite.fmId)
-				myFm && myFavoriteFm.push(myFm)
-			})
-			return {
-				allFm: fmDetails,
-				favoriteFm: myFavoriteFm
-			}
-		},
-
-		getNepaliEvent: (parent, { date }) => {
-			const year = date.slice(0, 4)
-			const month = parseInt(date.slice(5, 7))
-			const day = parseInt(date.slice(8))
-			const currentYear = NepaliEvents.find((x) => x.year == year)
-			const currentMonth = currentYear.months.find((x) => x.month == month)
-			const currentDay = currentMonth.days.find((x) => x.dayInEn == day)
-			return currentDay
-		},
-
-		getReadCategoryData: async (parent, args, {}) => {
-			//this week
-			const currentDate = new Date()
-			let oneWeekBeforeDate = new Date()
-			oneWeekBeforeDate.setDate(oneWeekBeforeDate.getDate() - 7) // one week before
-			const weekData = await ReadArticle.find({
-				'article.createdDate': {
-					$lte: currentDate,
-					$gte: oneWeekBeforeDate
-				}
-			})
-
-			let totalWeekArticles = []
-			weekData.forEach(singleUser=>{
-				const myArticle = singleUser.article || []
-				const weekArticles = myArticle.filter(x => x.createdDate<=currentDate && x.createdDate>=oneWeekBeforeDate)
-				totalWeekArticles = totalWeekArticles.concat(weekArticles)
-			})
-
-			let weekStats = []
-			categories.forEach(category => {
-				const myCatArticle = totalWeekArticles.filter(x => x.category == category.name).length
-				weekStats.push({
-					category: category.name,
-					data: myCatArticle
-				})
-			})
-
-			//this month
-			let oneMonthBeforeDate = new Date()
-			oneMonthBeforeDate.setMonth(oneMonthBeforeDate.getDate() - 30)
-			const monthData = await ReadArticle.find({
-				'article.createdDate': {
-					$lte: currentDate,
-					$gte: oneMonthBeforeDate
-				}
-			})
-
-			let totalMonthArticles = []
-			monthData.forEach(singleUser=>{
-				const myArticle = singleUser.article || []
-				const monthArticles = myArticle.filter(x => x.createdDate<=currentDate && x.createdDate>=oneMonthBeforeDate)
-				totalMonthArticles = totalMonthArticles.concat(monthArticles)
-			})
-
-			let monthStats = []
-			categories.forEach(category => {
-				const myCatArticle = totalMonthArticles.filter(x => x.category == category.name).length
-				monthStats.push({
-					category: category.name,
-					data: myCatArticle
-				})
-			})
-
-			return {
-				weekStat: weekStats,
-				monthStat: monthStats
-			}
-		}
 	},
 
 	Mutation: {
-		storeFcmToken: async (parent, args, { ipAddress }) => {
+		addStockToWatchList: async (parent, args, { ipAddress }) => {
 			const {
 				input: { nid, fcmToken, countryCode, timeZone, createdDate, modifiedDate },
 			} = args
@@ -275,119 +76,6 @@ module.exports = {
 				{ upsert: true },
 			)
 
-			return { success: !!response.ok }
-		},
-
-		saveFavorite: async (parent, args, {}) => {
-			const {
-				input: { nid, fmId },
-			} = args
-
-			const response = await FavoriteFM.update(
-				{ nid, fmId },
-				{
-					$set: {
-						nid,
-						fmId,
-					},
-				},
-				{ upsert: true },
-			)
-
-			return { success: !!response.ok }
-		},
-
-		deleteFavorite: async (parent, args, {}) => {
-			const {
-				input: { nid, fmId },
-			} = args
-
-			const response = await FavoriteFM.deleteOne({ nid, fmId })
-			return { success: !!response.ok }
-		},
-
-		saveReadArticle: async (parent, args, {}) => {
-			const {
-				input: { nid, articles },
-			} = args
-			const savedReadArticles = await ReadArticle.findOne({ nid })
-			if (savedReadArticles && savedReadArticles.nid) {
-				let allArticles = savedReadArticles.article.concat(articles)
-				allArticles = allArticles.map((article) => {
-					return { articleId: article.articleId, category: article.category }
-				})
-				allArticles = allArticles.filter((thing, index, self) => index === self.findIndex((t) => t.articleId === thing.articleId))
-				savedReadArticles.article = allArticles
-				const response = await savedReadArticles.save()
-				return { success: !!response.nid }
-			} else {
-				const readArticlesObj = new ReadArticle({ nid, article: articles })
-				const response = await readArticlesObj.save()
-				return { success: !!response.nid }
-			}
-		},
-
-		postLike: async (parent, args, {}) => {
-			const {
-				input: { nid, articleId, category },
-			} = args
-			const myArticle = await Article.findOne({ _id: articleId })
-			let likes = myArticle.likes || []
-			likes.push({ nid })
-			let dislikes = myArticle.dislikes || []
-			dislikes = dislikes.filter((x) => x.nid != nid)
-			myArticle.likes = likes
-			myArticle.dislikes = dislikes
-			await myArticle.save()
-
-			await Like.insertMany([{ nid, articleId, category }])
-			const response = await Dislike.deleteOne({ nid, articleId })
-			return { success: !!response.ok }
-		},
-
-		removeLike: async (parent, args, {}) => {
-			const {
-				input: { nid, articleId },
-			} = args
-			const myArticle = await Article.findOne({ _id: articleId })
-			let likes = myArticle.likes || []
-			likes = likes.filter((x) => x.nid != nid)
-			myArticle.likes = likes
-			await myArticle.save()
-
-			const response = await Like.deleteOne({ nid, articleId })
-			return { success: !!response.ok }
-		},
-
-		postDislike: async (parent, args, {}) => {
-			const {
-				input: { nid, articleId, category },
-			} = args
-			const myArticle = await Article.findOne({ _id: articleId })
-			let dislikes = myArticle.dislikes || []
-			dislikes.push({ nid })
-			let likes = myArticle.likes || []
-			likes = likes.filter((x) => x.nid != nid)
-			myArticle.likes = likes
-			myArticle.dislikes = dislikes
-			await myArticle.save()
-
-			await Dislike.insertMany([{ nid, articleId, category }])
-			const response = await Like.deleteOne({ nid, articleId })
-			return { success: !!response.ok }
-		},
-
-		removeDislike: async (parent, args, {}) => {
-			const {
-				input: { nid, articleId },
-			} = args
-			const myArticle = await Article.findOne({ _id: articleId })
-			let dislikes = myArticle.dislikes || []
-			dislikes = dislikes.filter((x) => x.nid != nid)
-			myArticle.dislikes = dislikes
-			await myArticle.save()
-
-			const response = await Dislike.deleteOne({ nid, articleId })
 			return { success: !!response.ok }
 		},
 	},
--- packages/server/src/jobs/job-runner/start-jobs.js
+++ packages/server/src/jobs/job-runner/start-jobs.js
@@ -2,40 +2,19 @@ require('dotenv').config()
 const Agenda = require('agenda')
 const logger = require('../../config/logger')
 
-const crawler = require('../NewsCrawlerTrigger/index')
-const notifier = require('../NotificationTrigger')
-const twitterJob = require('../TwitterTrigger')
-const newsChecker = require('../newsChecker')
+const stocksRefresher = require('../StocksRefresher/index')
 
 module.exports = async function () {
 	logger.info('starting jobs')
 
 	const agenda = new Agenda({ db: { address: process.env.DATABASE_URL } })
 
-	agenda.define('crawl articles', async () => {
-		logger.info('crawl articles job started')
-		crawler()
-	})
-
-	agenda.define('notify users', async () => {
-		logger.info('notify users job started')
-		notifier()
-	})
-
-	agenda.define('pull tweets', async () => {
-		logger.info('pull tweets job started')
-		twitterJob()
-	})
-
-	agenda.define('check news from sources', async () => {
-		logger.info('checking news job started')
-		newsChecker()
+	agenda.define('refresh stocks', async () => {
+		logger.info('stocks refresh job started')
+		stocksRefresher()
 	})
 
 	await agenda.start()
 
-	await agenda.every('30 minutes', 'crawl articles')
-	await agenda.every('5 minutes', 'notify users')
-	await agenda.every('20 minutes', 'pull tweets')
-	await agenda.every('24 hours', 'check news from sources')
+	await agenda.every('10 minutes', 'refresh stocks')
 }
